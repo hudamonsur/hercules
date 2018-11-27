8dcfe85950e691f84677d7dc7af17a68c00155c2
Monsur <mmhuda@memphis.edu>
2018-11-27 11:25:00 -0600

    Faust's(1951) equation added but not activated and vs, vp, density conversion equations from Boor (2016) and Brocher (2005) added

--- quake/forward/constants_bengalbasin.h
+++ quake/forward/constants_bengalbasin.h
@@ -11,23 +11,60 @@ double MAXLONG = 93.4; // maximum longitude of the extent of simulation region i
 int GRIDSTEP = 2000; // grid step of the binary files generated from the contours
 double DEGREETODIST = 111.01*1000; // degree to distance multiplier in meters
 
-double SEDIMENTDEPTH = 100; // fixed sediment depth assumed for the model in meters
+//double SEDIMENTDEPTH = 100; // fixed sediment depth assumed for the model in meters
 double APPROXCRUSTTHICKNESS = 20000; // average approximated crustal thickness
 int BH_INFLUENCE = 20; // borehole influence distance.
 char BHNAMES_LIST[] = "borehole_list.txt";
+
+// typedef struct eq_constants
+// {
+//     double c1, c2, c3, c4, c5;   
+// } eq_constants;
+
+// eq_constants VPTOROH; // equation for converting P-Wave velocity to density accodring to Nafe and Drake (1961)
+// eq_constants VPTOVS; // equation for converting P-wave velocity to S-wave velocity according to Brocher (2005)
+// eq_constants VSTOVP; // equation for converting S-wave velocity to P-wave velocity according to Brocher (2005)
+
+// void initiate_equations(){
+//     VPTOROH.c1 = 1.6612;
+//     VPTOROH.c2 = 0.4721;
+//     VPTOROH.c3 = 0.0671;
+//     VPTOROH.c4 = 0.0043;
+//     VPTOROH.c5 = 0.000106;
+
+//     VPTOVS.c1 = 0.7858; 
+//     VPTOVS.c2 = 1.2344;
+//     VPTOVS.c3 = 0.7949;
+//     VPTOVS.c4 = 0.1238;
+//     VPTOVS.c5 = 0.0064;
+
+//     VSTOVP.c1 = 0.9409;
+//     VSTOVP.c2 = 2.0947;
+//     VSTOVP.c3 = 0.8206;
+//     VSTOVP.c4 = 0.2683;
+//     VSTOVP.c5 = 0.0251;
+// }
+
 typedef struct layer_params {
-    double minvs, maxvs, minrho, maxrho, vpvsratio;
+    double minvs, maxvs, minrho, maxrho, vpvsratio, minage, maxage, constantk;
 } layer_params;
 
-const char * LAYER_NAMES[] = {
-    "sediment",
-    "dupitila",
-    "tipam",
-    "bokabil",
-    "bhuban",
-    "premiocene",
-    "precambrian",
-    "mantle",
+typedef struct boreholes {
+    char * name;
+    double lattitude;
+    double longitude;
+    FILE * bh;
+} boreholes;
+
+int LAYER_NAMES[] = {
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
 };
 
 const char * BIN_NAMES[] = {
@@ -42,11 +79,14 @@ const char * BIN_NAMES[] = {
 int BIN_COUNT = 7;
 int LAYER_COUNT = 8;
 layer_params LAYERS[8];
-double LAYER_MINVS[] = {100, 700, 1000, 1300, 1700, 2200, 3250, 4500};
-double LAYER_MAXVS[] = {500, 1000, 1300, 1700, 2200, 2400, 3500, 6000};
+double LAYER_MINVS[] = {100, 700, 1000, 1300, 1700, 2200, 3300, 4500};
+double LAYER_MAXVS[] = {500, 1000, 1300, 1700, 2200, 2400, 3900, 4500};
 double LAYER_MINRHO[] = {2000, 2100, 2200, 2350, 2450, 2550, 2670, 3300};
 double LAYER_MAXRHO[] = {2050, 2200, 2350, 2450, 2550, 2600, 2800, 3300};
 double LAYER_VPVSRATIO[] = {2.2, 2.1, 2.1, 2.0, 2.0, 1.9, 1.8, 1.7};
+double LAYER_MINAGE[] = {0, 1.5, 5.5, 11, 18, 24, 130, 300};
+double LAYER_MAXAGE[] = {1.5, 5.5, 11, 18, 24, 130, 300, 2000};
+double LAYER_CONSTANT[] = {150, 150, 150, 150, 150, 150, 150, 170};
 
 void initiate_layers(){
     int i = 0;
@@ -56,14 +96,59 @@ void initiate_layers(){
          LAYERS[i].minrho = LAYER_MINRHO[i];
          LAYERS[i].maxrho = LAYER_MAXRHO[i];
          LAYERS[i].vpvsratio = LAYER_VPVSRATIO[i];
+         LAYERS[i].minage = LAYER_MINAGE[i];
+         LAYERS[i].maxage = LAYER_MAXAGE[i];
+         LAYERS[i].constantk = LAYER_CONSTANT[i];
     }
 }
 
-// void initiate_globals(){
-//     initiate_layers();
+boreholes BOREHOLE_INFO[55];
+void initiate_boreholes(){
+    FILE * fp_bengalbasin;
+    fp_bengalbasin = fopen(BHNAMES_LIST,"r");
+    int k=0;
+    if (!fp_bengalbasin)
+    {
+        printf("Unable to open borehole list!");
+    }
+    else {
+        while(!feof(fp_bengalbasin)) {
+            char str[60];
+            if( fgets (str, 60, fp_bengalbasin)!=NULL ) {
+            /* writing content to stdout */
+//                if(str[0]=='\n'){
+//                    linecount++;
+//                }
+                char *borehole_name = strtok(str, " ");
+                char *borehole_lat = strtok(NULL, " ");
+                char *borehole_long = strtok(NULL, " ");
+//                puts(borehole_name);
+//                puts(boreholeinfo[1]);
+                double borehole_latf =  atof(borehole_lat);
+                double borehole_longf =  atof(borehole_long);
+
+                BOREHOLE_INFO[k].name = (char*) malloc(60 * sizeof(char));
+                strcpy(BOREHOLE_INFO[k].name, borehole_name);
+                BOREHOLE_INFO[k].lattitude = borehole_latf;
+                BOREHOLE_INFO[k].longitude = borehole_longf;
+
+                char str1[] = ".txt";
+                strcat(borehole_name, str1);
+                BOREHOLE_INFO[k].bh = fopen(borehole_name,"r");
+                k++;
+            }
+        }
+    }
+    fclose(fp_bengalbasin);
+
+}
+void initiate_globals(){
+    initiate_layers();
+    // initiate_boreholes();
+    // initiate_equations();
 
 
-// }
+}
 
 //LAYERS[0].minvs = 100; // minimum shear wave velocity of the quaternary sediment in meter/sec
 //LAYERS[0].maxvs = 500; // maximum shear wave velocity of the quaternary sediment in meter/sec
@@ -114,8 +199,8 @@ void initiate_layers(){
 //LAYERS[7].vpvsratio = 1.7; // ratio between P-wave and S-wave velocity of the Mantle
 FILE* fp_bengalbasin;
 FILE* fp_borehole;
+// int DEBUG = 0;
 int DB = 1;
-//int DEBUG = 0;
 
 //typedef struct loc {
 //    double laty, longx;
